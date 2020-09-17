c9652c78fbb754f322b321c028413d0354c39375
Jeremy Richards <jeremyrichards2289@gmail.com>
2020-09-17 19:44:41 +0200

    landing page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,6 @@ class PagesController < ApplicationController
   def home
   end
 
-  def about
+  def landing
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'pages#landing'
 
-  get 'about', to: 'pages#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
