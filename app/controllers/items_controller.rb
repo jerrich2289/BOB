a1713d65f60753c9deab68d6d6604a8260b3c138
Martin Lillebo <martin@pqa.no>
2020-09-19 10:27:41 +0200

    added basic components

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,10 @@
+class ItemsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'items/new'
+  get 'items/create'
+  get 'items/index'
   devise_for :users
   root to: 'pages#landing'
 
