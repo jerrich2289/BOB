169dbc53528f8dd1de1a195c0a06ff7a25e504ca
Martin Lillebo <martin@pqa.no>
2020-09-22 21:27:46 +0200

    Created styling file and 50% of the "Add item" page

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,17 @@
 class ItemsController < ApplicationController
   def new
+    @item = Item.new
   end
 
+
+  # item-infoen kommer inn til create som innholdet inni params. 
+  # Dette må så filtreres gjennom strong_params før det tildeles 
+  # new_item. 
   def create
+    @new_item = Item.new(strong_params)
+    @new_item.owner = current_user
+    @new_item.save!
+    redirect_to items_path
   end
 
   def index
@@ -12,4 +21,11 @@ class ItemsController < ApplicationController
     # this one's just for making the card 
     # component dummy site function
   end
+
+  private
+
+  def strong_params
+    params.require(:item).permit(:name, :description, :photo)
+  end
+
 end
