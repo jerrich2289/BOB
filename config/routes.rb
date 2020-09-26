e47b2d329b935af6aef56275442b9f4cf04bb89a
Martin Lillebo <martin@pqa.no>
2020-09-26 14:55:51 +0200

    basic show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :items, only: [:new, :create, :index]
+  get 'items/dummy'
+  resources :items, only: [:new, :create, :index, :show]
   devise_for :users
   root to: 'pages#landing'
 
-  get 'items/dummy'
   get 'trades/new'
   get '/user' => "items#index", :as => :user_root
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
