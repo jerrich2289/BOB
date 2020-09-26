4d7c9178d94e1ae03266cb674bde1bf4e866ab40
Leif Johansen <leif@leifjohansen.com>
2020-09-26 12:35:59 +0200

    can upload many photos

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ApplicationRecord
   belongs_to :owner, class_name: 'User', foreign_key: 'owner_id'
   has_many_attached :photos
   before_save :image_count
-  validates :photo, presence: true
+  validates :photos, presence: true
   validates :name, presence: true
   # validates :size, presence: true (once the sizing gets done then the turn on the validation)
 
