b916faae0e7cbfd93520463efd4ec128d5e5389c
Leif Johansen <leif@leifjohansen.com>
2020-10-03 15:06:51 +0200

    spacing

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,8 @@ class User < ApplicationRecord
   has_many :items, foreign_key: 'owner_id'
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-def trades
-  item_ids=items.pluck(:id)
-  Trade.where(item_1_id:item_ids).or(Trade.where(item_2_id:item_ids))
+  def trades
+    item_ids = items.pluck(:id)
+    Trade.where(item_1_id:item_ids).or(Trade.where(item_2_id:item_ids))
+  end
 end
-end
-
