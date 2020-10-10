94274a3304897d4412daea2f2cb3d3a4480de671
Martin Lillebo <martin@pqa.no>
2020-10-10 11:27:48 +0200

    fixed broken show page

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -16,11 +16,11 @@ class ItemsController < ApplicationController
     @items = Item.all
   end
 
-  # def show
-  #   @item = Item.find(params[:id])
-  #   @user = User.find(@item.owner_id)
-  #   redirect_to '/items/new', :notice => "Your item has been deleted"
-  # end
+  def show
+    @item = Item.find(params[:id])
+    @user = User.find(@item.owner_id)
+    redirect_to '/items/new', :notice => "Your item has been deleted"
+  end
 
   def destroy
     @item = Item.find(params[:id])
